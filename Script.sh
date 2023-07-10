ff5f3c5c54ac5fab6c9406f5fa3c5956067234fd
Gopala-Polimetla <91300247+Gopala-Polimetla@users.noreply.github.com>
2023-07-10 11:01:03 +0530

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
+# Get the current date and time
+datetime=$(date +"%Y-%m-%d %H:%M:%S")
+
+# Define the file path to store the date and time
+file_path="/var/lib/jenkins/workspace/Demo1/Date.txt"
+
+# Print the date and time to the file
+echo "$datetime" > "$file_path"
+
+# Display a success message
+echo "Date and time saved to $file_path"
 
