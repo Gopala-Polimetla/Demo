5600198c0a59d7c455fb3468101726c11748a208
Gopala-Polimetla <91300247+Gopala-Polimetla@users.noreply.github.com>
2023-12-22 12:35:44 +0530

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -4,7 +4,7 @@
 datetime=$(date +"%Y-%m-%d %H:%M:%S")
 
 # Define the file path to store the date and time
-file_path="/var/lib/jenkins/workspace/Demo1/Date.txt"
+file_path="/var/lib/jenkins/workspace/Sample/Date.txt"
 
 # Print the date and time to the file
 echo "$datetime" > "$file_path"
